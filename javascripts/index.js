9eba06cd2955fca0778e357671477fb3af5cb52b
sdzharkov <stephan.zharkov@gmail.com>
2016-06-23 21:52:46 -0700

    more

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,18 +1,5 @@
-/**
- * Listen to scroll to change header opacity class
- */
-function checkScroll(){
-    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
-
-    if($(window).scrollTop() > startY){
-        $('.navbar').addClass("scrolled");
-    }else{
-        $('.navbar').removeClass("scrolled");
-    }
-}
-
-if($('.navbar').length > 0){
-    $(window).on("scroll load resize", function(){
-        checkScroll();
+$(document).ready(function() {
+    $('.bar').click(function() {
+        $('.bar').css("height", "auto").slideToggle('slow');
     });
-}
+});
